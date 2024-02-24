f8c166bdb8b9b11a8216bf88675a4b3f6859cc64
Andrew <121783441+Crash-Sasek@users.noreply.github.com>
2024-02-24 14:50:48 -0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "7111394408950485",
-    clientSecret: "83f1e6fcd1247ffa2ee9c01100c03899",
-  },
-};
